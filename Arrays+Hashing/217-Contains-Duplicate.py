f7c5ca8b54c302998e3d2fa06da12c5379f053b8
Thomas Killian <tkillia2@nd.edu>
2022-06-21 19:16:58 -0400

    Update 217-Contains-Duplicate.py

--- Arrays+Hashing/217-Contains-Duplicate.py
+++ Arrays+Hashing/217-Contains-Duplicate.py
@@ -1,3 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return False if len(set(nums)) == len(nums) else True
+'''
+For this problem shoving the list into a set is sufficient for checking for duplicates.
+this is b/c a set will only keep one of each value so [1,2,1] as a list would become [1,2] as a set
+then checking their lengths against each other will determine if there are duplicates or not
+'''
